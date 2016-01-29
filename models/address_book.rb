728c275327d29bce6821dc706030dd5fe289a5ee
Sharrell Porter <SharrellPorter@example.com>
2016-01-29 10:07:35 -0500

    assignment-19

--- models/address_book.rb
+++ models/address_book.rb
@@ -18,4 +18,6 @@ class AddressBook
     end
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
+  def remove_entry(name, phone_number, email)
+  end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -33,5 +33,11 @@ RSpec.describe AddressBook do
       expect(new_entry.phone_number).to eq('010.012.1815')
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
-  end
+    it "deletes passed entry" do
+      expect(@book.entries.size).to eq(2)
+      @book.remove_entry('Sarah Smith', '435.724.5567', 'eatchicken@home.com')
+      expect(@book.entries.size).to eq(1)
+      @book.remove_entry('Henry Gomes', '568.435.9834', 'hungrylife@food.com')
+      expect(@book.entries.size).to eq(0)
+    end
 end
