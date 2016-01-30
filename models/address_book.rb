796d8760d29e0b63575faaf5095b0bacde982b72
Sharrell Porter <SharrellPorter@example.com>
2016-01-30 08:44:36 -0500

    assignment-22

--- models/address_book.rb
+++ models/address_book.rb
@@ -54,4 +54,14 @@ class AddressBook
     end
     return nil
   end
+  def iterative_search(name)
+    index = 0
+    while index < entries.length
+      if name == entries[index].name
+        return entries[index]
+      end
+      index += 1
+    end
+    return nil
+  end
 end
