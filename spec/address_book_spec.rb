3d7257c72776993e01b6d14d964d644bec7ecc3d
Sharrell Porter <SharrellPorter@example.com>
2016-01-30 07:43:17 -0500

    assignment-21

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -39,13 +39,7 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
-  describe "#import_from_csv" do
-       it "imports the correct number of entries" do
-         book.import_from_csv("entries.csv")
-         book_size = book.entries.size
 
-         expect(book_size).to eq 5
-       end
        it "imports the 1st entry" do
           book.import_from_csv("entries.csv")
           entry_one = book.entries[0]
@@ -74,7 +68,7 @@ RSpec.describe AddressBook do
           entry_five = book.entries[4]
           check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
         end
-      end
+
   describe "#remove_entry" do
     before :each do
       @book = AddressBook.new
@@ -89,6 +83,30 @@ RSpec.describe AddressBook do
       @book.remove_entry('Henry Gomes', '568.435.9834', 'hungrylife@food.com')
 
       expect(@book.entries.size).to eq(0)
+    end
+  end
+  describe ".import_from_csv with entries_2.csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+
+      expect(book_size).to eq 3
+      end
+
+    it "imports the 1st entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_one = book.entries[0]
+      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+    end
+    it "imports the 2nd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_two = book.entries[1]
+      check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+    end
+    it "imports the 3rd entry" do
+      book.import_from_csv("entries_2.csv")
+      entry_three = book.entries[2]
+      check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
     end
   end
 end
