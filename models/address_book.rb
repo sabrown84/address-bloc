88df46e72151641cb9f747f999d2ca3395995a5e
Sharrell Porter <SharrellPorter@example.com>
2016-01-30 07:01:27 -0500

    checkpoint-21

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,6 +7,7 @@ class AddressBook
   def initialize
     @entries = []
   end
+
   def add_entry(name, phone_number, email)
 
     index = 0
@@ -22,13 +23,19 @@ class AddressBook
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
      csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
- 
+
      csv.each do |row|
        row_hash = row.to_hash
        add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
      end
   end
-end
+
   def remove_entry(name, phone_number, email)
-  end
+   @entries.each_with_index do |value, index|
+     if value.name == name && value.phone_number == phone_number && value.email == email
+       @entries.delete_at(index)
+       break
+     end
+   end
+ end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,18 +1,17 @@
 require_relative '../models/address_book'
 
+
 RSpec.describe AddressBook do
 
-  let(:book) {AddressBook.new}
-
-def check_entry(entry, expect_name, expected_number, expected_email)
-  expect(entry.name).to eq expected_name
-  expect(entry.phone_number).to eq expected number
-  expect(entry.email).to eq expected_email
-end
+  let (:book) { AddressBook.new }
+  def check_entry(entry, expected_name, expected_number, expected_email)
+      expect(entry.name).to eq expected_name
+      expect(entry.phone_number).to eq expected_number
+      expect(entry.email).to eq expected_email
+  end
 
   describe "attributes" do
     it "should respond to entries" do
-
       expect(book).to respond_to(:entries)
     end
 
@@ -24,6 +23,7 @@ end
       expect(book.entries.size).to eq(0)
     end
   end
+
   describe "#add_entry" do
     it "adds only one entry to the address book" do
       book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
@@ -39,61 +39,56 @@ end
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
-
   describe "#import_from_csv" do
-    it "imports the correct number of entries" do
+       it "imports the correct number of entries" do
+         book.import_from_csv("entries.csv")
+         book_size = book.entries.size
 
-      book.import_from_csv("entries.csv")
-      book_size = book.entries.size
+         expect(book_size).to eq 5
+       end
+       it "imports the 1st entry" do
+          book.import_from_csv("entries.csv")
+          entry_one = book.entries[0]
+          check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+        end
+        it "imports the 2nd entry" do
+          book.import_from_csv("entries.csv")
+          entry_two = book.entries[1]
+          check_entry(entry_two, "Bob", "555-555-5415", "bob@blocmail.com")
+        end
 
-      expect(book_size).to eq 5
+        it "imports the 3rd entry" do
+          book.import_from_csv("entries.csv")
+          entry_three = book.entries[2]
+          check_entry(entry_three, "Joe", "555-555-3660", "joe@blocmail.com")
+        end
+
+        it "imports the 4th entry" do
+          book.import_from_csv("entries.csv")
+          entry_four = book.entries[3]
+          check_entry(entry_four, "Sally", "555-555-4646", "sally@blocmail.com")
+        end
+
+        it "imports the 5th entry" do
+          book.import_from_csv("entries.csv")
+          entry_five = book.entries[4]
+          check_entry(entry_five, "Sussie", "555-555-2036", "sussie@blocmail.com")
+        end
+      end
+  describe "#remove_entry" do
+    before :each do
+      @book = AddressBook.new
+      @book.add_entry('Sarah Smith', '435.724.5567', 'eatchicken@home.com')
+      @book.add_entry('Henry Gomes', '568.435.9834', 'hungrylife@food.com')
     end
-    it "imports the 1st entry" do
-      book.import_from_csv("entries.csv")
-      entry_one = book.entries[0]
-      check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
+
+    it "deletes passed entry" do
+      expect(@book.entries.size).to eq(2)
+      @book.remove_entry('Sarah Smith', '435.724.5567', 'eatchicken@home.com')
+      expect(@book.entries.size).to eq(1)
+      @book.remove_entry('Henry Gomes', '568.435.9834', 'hungrylife@food.com')
+
+      expect(@book.entries.size).to eq(0)
     end
-
-    it "imports the 2nd entry" do
-       book.import_from_csv("entries.csv")
-       entry_two = book.entries[1]
-       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
-     end
-
-     it "imports the 3rd entry" do
-       book.import_from_csv("entries.csv")
-       entry_three = book.entries[2]
-       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
-     end
-
-     it "imports the 4th entry" do
-       book.import_from_csv("entries.csv")
-       entry_four = book.entries[3]
-       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
-     end
-
-     it "imports the 5th entry" do
-       book.import_from_csv("entries.csv")
-       entry_five = book.entries[4]
-       check_entry(entry_one, "Bill", "555-555-4854", "bill@blocmail.com")
-     end
-
   end
-end
-
-
-    describe "#remove_entry" do
-      before :each do
-        @book = AddressBook.new
-        @book.add_entry('Sarah Smith', '435.724.5567', 'eatchicken@home.com')
-        @book.add_entry('Henry Gomes', '568.435.9834', 'hungrylife@food.com')
-      end
-
-      it "deletes passed entry" do
-        expect(@book.entries.size).to eq(2)
-        @book.remove_entry('Sarah Smith', '435.724.5567', 'eatchicken@home.com')
-        @book.remove_entry('Henry Gomes', '568.435.9834', 'hungrylife@food.com')
-
-        expect(@book.entries.size).to eq(2)
-      end
 end
